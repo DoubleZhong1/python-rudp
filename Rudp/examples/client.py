c65735bbbc74e4d2e7c782fac52b0a004496cd49
Brian Hoffman <brianchoffman@gmail.com>
2014-11-30 09:45:51 -0500

    Rename files

---
+++ Rudp/examples/client.py
@@ -0,0 +1,26 @@
+__author__ = 'brianhoffman'
+
+import socket
+import sys
+
+from Rudp.client import Client
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+client = Client(sock, 'localhost', 12345)
+
+while 1:
+    try:
+        data = sys.stdin.readline()
+    except KeyboardInterrupt:
+        break
+
+    if not data:
+        break
+
+    data = data.encode('utf-8')
+
+    client.send(data)
+    print data.strip()
+
